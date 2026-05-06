c0dc33d382b0ea2696b610c706a1dcd4e4960086
Kareemah-codes <krimmyninja@gmail.com>
2026-05-06 14:10:53 +0100

    error correction

--- angry_professor.py
+++ angry_professor.py
@@ -1,7 +1,7 @@
 def angryProfessor(k,a):
     students_on_time =0
     for time in a:
-        if time <0:
+        if time <=0:
             students_on_time +=1
     if students_on_time >= k:
         return 'NO'
